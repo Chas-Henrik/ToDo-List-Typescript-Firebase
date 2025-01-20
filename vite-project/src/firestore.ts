4f69b34b09098b350e956ef39fcd0d0a55169532
Henrik Suurik <henrik.suurik@chasacademy.se>
2025-01-20 14:03:12 +0100

    Added firebaseConfig

--- vite-project/src/firestore.ts
+++ vite-project/src/firestore.ts
@@ -8,17 +8,12 @@ import { Auth, getAuth } from "firebase/auth";
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-    apiKey: "API_KEY",
-    authDomain: "PROJECT_ID.firebaseapp.com",
-    // The value of `databaseURL` depends on the location of the database
-    databaseURL: "https://DATABASE_NAME.firebaseio.com",
-    projectId: "PROJECT_ID",
-    // The value of `storageBucket` depends on when you provisioned your default bucket (learn more)
-    storageBucket: "PROJECT_ID.firebasestorage.app",
-    messagingSenderId: "SENDER_ID",
-    appId: "APP_ID",
-    // For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
-    measurementId: "G-MEASUREMENT_ID",
+    apiKey: "AIzaSyDkLzs8sAZ1dlk2-kRa2KMcb01wwqBVfrk",
+    authDomain: "todo-list-64101.firebaseapp.com",
+    projectId: "todo-list-64101",
+    storageBucket: "todo-list-64101.firebasestorage.app",
+    messagingSenderId: "708703569768",
+    appId: "1:708703569768:web:84f83f52c173235241b8dd"
 };
 
 // Initialize Firebase
